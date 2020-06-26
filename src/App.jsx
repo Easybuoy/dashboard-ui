11f9c1a8d12086eb8581136e47255d0c33256cbc
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-26 04:53:19 +0100

    setup file structure

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/register" component={Signup} />
+        <Route exact path="/login" component={Signin} />
+        <Route exact path="/" component={Dashboard} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
