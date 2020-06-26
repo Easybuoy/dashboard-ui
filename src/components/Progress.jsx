a2a4cc37378d50ace6520366bf29e17b6b1749cc
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-26 18:43:49 +0100

    add sales by country markup

---
+++ src/components/Progress.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledProgress = styled.div`
+  .progress {
+    margin-top: 1rem;
+    background-color: #e5e9f2;
+    height: 0.2em;
+    position: relative;
+    width: 100%;
+    border-radius: 10px;
+  }
+  .progress-bar {
+    background-size: 23em 0.25em;
+    height: 100%;
+    width: 70%;
+    position: relative;
+    border-radius: 10px;
+
+  }
+`;
+
+const Progress = ({ color }) => {
+  return (
+    <StyledProgress>
+      <div className="progress">
+        <div className="progress-bar" style={{ backgroundColor:color }}></div>
+      </div>
+    </StyledProgress>
+  );
+};
+
+export default Progress;
--- src/components/ui/SalesByCountry.jsx
+++ src/components/ui/SalesByCountry.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import { ResponsiveGeoMap } from "@nivo/geo";
 
 import Button from "../Button";
+import Progress from "../Progress";
+import Features from "../../data/features";
 
 const StyledSalesByCountry = styled.div`
   background-color: white;
-  height: 300px;
   width: 65%;
   border-radius: 5px;
 
@@ -15,6 +17,54 @@ const StyledSalesByCountry = styled.div`
     justify-content: space-between;
     padding: 1rem;
     align-items: center;
+  }
+
+  .content {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding: 0 1rem;
+
+    .chart {
+      width: 45%;
+      /* height: 600px; */
+
+      div {
+        height: 300px !important;
+        svg {
+        }
+      }
+
+      .option {
+        height: auto !important;
+      }
+    }
+
+    .details {
+      width: 45%;
+
+      .content-detail-container {
+        margin: 2rem 0;
+      }
+      .content-detail {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+
+        p {
+          margin: 0;
+          font-size: 0.8rem;
+          font-weight: bold;
+        }
+
+        span {
+          color: #5e5e75;
+          font-size: 0.8rem;
+          font-weight: bold;
+        }
+      }
+    }
   }
 `;
 
@@ -26,6 +76,68 @@ const SalesByCountry = () => {
         <Button>View Full Report</Button>
       </div>
       <hr />
+
+      <div className="content">
+        <div className="chart">
+          <ResponsiveGeoMap
+            features={Features}
+            margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            projectionScale={114}
+            projectionTranslation={[0.7, 0.7]}
+            projectionRotation={[0, 0, 0]}
+            fillColor="#eeeeee"
+            borderWidth={0.5}
+            borderColor={{ theme: "background" }}
+            graticuleLineWidth={2}
+            graticuleLineColor="#666666"
+          />
+
+          <div className="option">
+            <p>Plus</p>
+          </div>
+        </div>
+        <div className="details">
+          <div className="content-detail-container">
+            <div className="content-detail">
+              <p>Russia</p>
+              <span>$203,000</span>
+            </div>
+            <Progress color="#7764e4" />
+          </div>
+
+          <div className="content-detail-container">
+            <div className="content-detail">
+              <p>Australia</p>
+              <span>$203,000</span>
+            </div>
+            <Progress color="#32ce97" />
+          </div>
+
+          <div className="content-detail-container">
+            <div className="content-detail">
+              <p>China</p>
+              <span>$203,000</span>
+            </div>
+            <Progress color="#feb969" />
+          </div>
+
+          <div className="content-detail-container">
+            <div className="content-detail">
+              <p>Algeria</p>
+              <span>$203,000</span>
+            </div>
+            <Progress color="#f53c56" />
+          </div>
+
+          <div className="content-detail-container">
+            <div className="content-detail">
+              <p>United States</p>
+              <span>$203,000</span>
+            </div>
+            <Progress color="#fb633f" />
+          </div>
+        </div>
+      </div>
     </StyledSalesByCountry>
   );
 };
